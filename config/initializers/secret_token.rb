3b108d5b77a1a828e8270550a595959c80793345
Anezio Campos <newdevas@gmail.com>
2012-10-23 17:45:07 -0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juntus::Application.config.secret_token = '633e0b1f3cb97f6ba189ba883c625d7858b9f943c4b24178ef898a1f364b8b3f7d2f8db8a487a5ecb8fe5809ec08d89d8f46a407c77ff537acfc23c48c826154'
