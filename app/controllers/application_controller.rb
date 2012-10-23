88b697b98eb2923ce7c1afe89b934bf49285dd2f
Anezio Campos <newdevas@gmail.com>
2012-10-23 20:07:58 -0200

    Commented unused methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,15 @@ class ApplicationController < ActionController::Base
     session[:return_path] = request.path
   end
   
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def user_signed_in?
-    !current_user.nil?
-  end
-
-  def authenticate!
-    user_signed_in? || redirect_to((request.env["HTTP_REFERER"].blank?) ? root_path : :back, :notice => "Você precisa estar logado...")
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  # 
+  # def user_signed_in?
+  #   !current_user.nil?
+  # end
+  # 
+  # def authenticate!
+  #   user_signed_in? || redirect_to((request.env["HTTP_REFERER"].blank?) ? root_path : :back, :notice => "Você precisa estar logado...")
+  # end
 end
