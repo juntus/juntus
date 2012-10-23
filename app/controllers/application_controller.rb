f482bfb964882b4a23ceabcd173ce9536851fcbb
Anezio Campos <newdevas@gmail.com>
2012-10-23 20:25:24 -0200

    Add facebook login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
   
   private
   def save_return_path
     session[:return_path] = request.path
   end
   
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-  # 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   # def user_signed_in?
   #   !current_user.nil?
   # end
